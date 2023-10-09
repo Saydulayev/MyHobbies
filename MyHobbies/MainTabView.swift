5240c660178b5d485f7e2d2b91ab7213d5f1868f
Saydulayev <saydulayev.wien@gmail.com>
2023-10-09 20:08:38 +0200

    Firebase integration is underway, and many other tasks have been added during these changes

---
+++ MyHobbies/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  MyHobbies
+//
+//  Created by Akhmed on 09.10.23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
